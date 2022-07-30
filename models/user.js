af29e5124587541ed1fe28b4f37f6f78a41ecbe1
Foxylab <rushan.shakirzianov@gmail.com>
2022-07-30 14:53:29 +0300

    hotfix: add validation message in models

--- models/user.js
+++ models/user.js
@@ -4,14 +4,14 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Must be at least 2, you got {VALUE}'],
+    maxlength: [30, 'Must be no more than 30, you got {VALUE}'],
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Must be at least 2, you got {VALUE}'],
+    maxlength: [30, 'Must be no more than 30, you got {VALUE}'],
   },
   avatar: {
     type: String,
