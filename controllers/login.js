2f1b72e3a1bc83cc75299b61f29514fe84543511
Foxylab <rushan.shakirzianov@gmail.com>
2022-08-03 20:20:17 +0300

    refactor: make function findUserByCredentials

--- controllers/login.js
+++ controllers/login.js
@@ -1,25 +1,35 @@
-const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const { UNAUTHORIZED } = require('../utils/statuses');
 
 module.exports.login = (req, res) => {
   const { email, password } = req.body;
 
-  User.findOne({ email })
+  return User.findUserByCredentials(email, password)
     .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
-      return bcrypt.compare(password, user.password);
-    })
-    .then((matched) => {
-      if (!matched) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
-      return res.send({ token: 'Всё верно!' });
+      res.send(user);
     })
     .catch((err) => {
       res
-        .status(401)
+        .status(UNAUTHORIZED)
         .send({ message: err.message });
     });
 };
+
+/* User.findOne({ email })
+  .then((user) => {
+    if (!user) {
+      return Promise.reject(new Error('Неправильные почта или пароль'));
+    }
+    return bcrypt.compare(password, user.password);
+  })
+  .then((matched) => {
+    if (!matched) {
+      return Promise.reject(new Error('Неправильные почта или пароль'));
+    }
+    return res.send({ token: 'Всё верно!' });
+  })
+  .catch((err) => {
+    res
+      .status(UNAUTHORIZED)
+      .send({ message: err.message });
+  }); */
