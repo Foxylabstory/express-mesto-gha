73f82991d39a5bfd2bb2e5254affd97fc9bc6fc3
Foxylab <rushan.shakirzianov@gmail.com>
2022-08-05 17:59:03 +0300

    feat: hide password from requests to model

--- models/user.js
+++ models/user.js
@@ -35,15 +35,18 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'password is required'],
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
       }
+      console.log(`password from request: ${password}`);
+      console.log(`password from response: ${user.password}`);
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
